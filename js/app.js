890375993a0b4efeb71cd5a7df59d1a8db2a6eac
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2015-11-22 21:46:20 +0100

    Adjusting the navbar

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ window.addEventListener('DOMContentLoaded', function() {
   function start() {
     
     $('.button-collapse').sideNav({
-      menuWidth: 240, // Default is 240
+      menuWidth: 200, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
