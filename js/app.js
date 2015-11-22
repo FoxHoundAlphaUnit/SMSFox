820697f206dae74368b3d83f82000fd66076680f
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2015-11-22 18:27:07 +0100

    Adding functional Material Design icons to the app

--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@ window.addEventListener('DOMContentLoaded', function() {
   function start() {
     
     $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
+      menuWidth: 240, // Default is 240
+      edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
 
