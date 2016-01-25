49661064f6cbf444c084386783445a91c37749c1
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2016-01-25 11:30:52 +0100

    Cleaning a bit

--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
-	QUnit.test("hello test", function(assert) {
+	QUnit.test("Hello test", function(assert) {
 	  assert.ok(1 == "1", "Passed!");
 	});
 });
