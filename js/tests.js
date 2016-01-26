526539c3706dd3d4629ff9f421dc4ba59bc36647
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2016-01-26 21:35:58 +0100

    Test examples (test session tomorrow morning)

--- js/tests.js
+++ js/tests.js
@@ -1,5 +1,13 @@
 window.addEventListener('DOMContentLoaded', function() {
-	QUnit.test("Hello test", function(assert) {
-	  assert.ok(1 == "1", "Passed!");
+	QUnit.test("Opened server", function(assert){
+		assert.ok(1 == "1", "Passed!");
+	});
+	
+	QUnit.test("Closed server", function(assert){
+		assert.ok(1 == "1", "Passed!");
+	});
+	
+	QUnit.test("...", function(assert){
+		assert.ok(1 == "1", "Passed!");
 	});
 });
